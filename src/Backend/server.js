3dce7dac902668f0efa529bb1f45a4a9477fe412
BanhiDani <sistbroo@gmail.com>
2026-05-28 11:32:38 +0200

    gemini beimportálása

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const FakeAIService = require("./Ai/FakeAIService");
 const cors = require("cors");
+require("dotenv").config();
 
 // ===== APP LÉTREHOZÁS =====
 const app = express();
@@ -15,7 +16,11 @@ app.use(cors());
 app.use(express.json());
 
 // ===== AI PÉLDÁNY =====
-const aiService = new FakeAIService();
+
+const GeminiAIService = require("./ai/GeminiAIService");
+
+const aiService = new GeminiAIService(process.env.GEMINI_API_KEY)
+
 
 // ===== HEALTH CHECK =====
 app.get("/api/health", (req, res) => {
@@ -26,7 +31,7 @@ app.get("/api/health", (req, res) => {
 });
 
 // ===== CHAT ENDPOINT =====
-app.post("/api/chat", (req, res) => {
+app.post("/api/chat", async (req, res) => {
   const { language, level, message } = req.body;
 
   const reply = aiService.reply({
